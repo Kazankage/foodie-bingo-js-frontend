34763b452b92015076091f76fe4dbfec3bd041e2
github username <github email address>
2020-01-13 11:04:14 -0800

    adapter/app.js

---
+++ src/adapters/BingosAdapter.js
@@ -0,0 +1,5 @@
+class RestaurantsAdapter {
+    constructor() {
+        this.baseUrl = 'http://localhost:3000/bingos'
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+fetch('http://localhost:3000/bingos')
+.then(res => res.json())
+.then(bingos => {
+    bingos.forEach(hog => {
+
+    })
+})
