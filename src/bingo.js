c2fd9111d8eb0f54c1707193af78a51583659685
github username <github email address>
2020-01-13 11:35:30 -0800

    index.js/bingo class

---
+++ src/bingo.js
@@ -0,0 +1,16 @@
+class bingo {
+    constructor(id, name, city, country, specialty, price, rating, hype){
+        this.id = id
+        this.name = name
+        this.city = city
+        this.country = country
+        this.specialty = specialty
+        this.price = price
+        this.rating = rating
+        this.hype = hype
+        this.render()
+      }
+    
+
+
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 fetch('http://localhost:3000/bingos')
 .then(res => res.json())
 .then(bingos => {
-    bingos.forEach( => {
+    bingos.forEach(bingo => 
+        
+        const bingo = { id, name, city, country, specialty, price, rating, hype }
 
-    })
+    )
 })
